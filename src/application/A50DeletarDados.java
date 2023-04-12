26b775b9ff90ce964e1cb12c26ed64c9f86613a1
João Silva Cruz <100077439+joaosilvacruz@users.noreply.github.com>
2023-04-11 21:54:43 -0300

    criado: deletar dados

--- src/application/A50DeletarDados.java
+++ src/application/A50DeletarDados.java
@@ -1,7 +1,39 @@
 package application;
 
+import db.DB;
+import db.DbIntegrityException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class A50DeletarDados {
     public static void main(String[] args) {
+        Connection conn = null;
+        PreparedStatement st = null;
+
+        try {
+            // A forma de DELETAR os Dados é a mesma de inserir os dados
+            // Tendo como diferença aquela a SQL Query
+            conn = DB.getConnection();
+
+            st = conn.prepareStatement(
+                    "DELETE FROM department"
+                    + "WHERE "
+                    + "Id = ?");
+
+            st.setInt(1, 2);
+
+            int rowsAffected = st.executeUpdate();
+
+            System.out.println("Done! Rows affected: " + rowsAffected);
+
+        } catch (SQLException e){
+            throw new DbIntegrityException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
 
     }
 }
